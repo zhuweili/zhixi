19f9e8348f444ccacab0bfc326ee6baa69a14195
weilizhu <weilizhu@buffalo.edu>
2018-02-02 00:24:11 -0800

    hh

--- app/routes/rest.js
+++ app/routes/rest.js
@@ -7,51 +7,7 @@ var listService = require('../service/listService');
 
 
 
-router.get('/publish_list/:page_id/:token', function(req, res) {
-    var page_id = req.params.page_id;
-    var token = req.params.token;
-    listService.get_publish_list(page_id, token, function(post) {
-        console.log(post);
-        res.send(post);
-    })
-});
 
-
-router.get('/unpublish_list/:page_id/:token', function(req, res) {
-    var page_id = req.params.page_id;
-    var token = req.params.token;
-    listService.get_unpublish_list(page_id, token, function(post) {
-        console.log(post);
-        res.send(post);
-    })
-});
-
-
-
-
-router.get('/post_num/:page_id/:token', function(req, res) {
-    var page_id = req.params.page_id;
-    var token = req.params.token;
-    listService.get_post_view_num(page_id, token, function(post) {
-        res.send(post);
-    })
-});
-
-
-router.post('/post_public', jsonParser, function(req, res) {
-
-    listService.post_public(req.body,function(post) {
-        res.send(post);
-    })
-});
-
-
-router.post('/post_unpublic', jsonParser, function(req, res) {
-
-    listService.post_unpublic(req.body,function(post) {
-        res.send(post);
-    })
-});
 
 router.post('/post_list', jsonParser, function(req, res) {
 
--- app/service/listService.js
+++ app/service/listService.js
@@ -1,126 +1,6 @@
-var request = require('request');
 var _ = require('lodash');
 var ListModel = require('../models/listModel');
 
-var get_publish_list = function(page_id, token, callback) {
-    var service_url = "https://graph.facebook.com" +  "/v2.11/" + page_id + "/posts?access_token=" + token;
-    var options = {
-        method: 'GET',
-        url: service_url ,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    function cb(error, response, body) {
-        if (!error) {
-            var info = JSON.parse(JSON.stringify(body));
-            callback(info);
-        }
-        else {
-            console.log('Error happened: '+ error);
-        }
-    }
-    request(options, cb);
-};
-
-
-var get_unpublish_list = function(page_id, token, callback) {
-    var service_url = "https://graph.facebook.com" +  "/v2.11/" + page_id + "/promotable_posts?is_published=false&access_token=" + token;
-    var options = {
-        method: 'GET',
-        url: service_url ,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    function cb(error, response, body) {
-        if (!error) {
-            var info = JSON.parse(JSON.stringify(body));
-            callback(info);
-        }
-        else {
-            console.log('Error happened: '+ error);
-        }
-    }
-    request(options, cb);
-};
-
-
-
-var get_post_view_num = function(post_id, token, callback) {
-    var service_url = "https://graph.facebook.com" +  "/v2.11/" + post_id + "/insights/post_engagements/lifetime?access_token=" + token;
-    var options = {
-        method: 'GET',
-        url: service_url ,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    function cb(error, response, body) {
-        if (!error) {
-            var info = JSON.parse(JSON.stringify(body));
-            callback(info);
-        }
-        else {
-            console.log('Error happened: '+ error);
-        }
-    }
-    request(options, cb);
-};
-
-
-
-var post_public = function(input, callback) {
-    console.log(input);
-    var service_url = "https://graph.facebook.com" +  "/v2.11/" + input.page_id + "/feed?message=" + input.text + "&access_token=" + input.token;
-    var options = {
-        method: 'POST',
-        url: service_url ,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    function cb(error, response, body) {
-        if (!error) {
-            var info = JSON.parse(JSON.stringify(body));
-            callback(info);
-        }
-        else {
-            console.log('Error happened: '+ error);
-        }
-    }
-    request(options, cb);
-};
-
-
-
-
-var post_unpublic = function(input, callback) {
-    console.log(input);
-    var service_url = "https://graph.facebook.com" +  "/v2.11/" + input.page_id + "/feed?published=false&message=" + input.text + "&access_token=" + input.token;
-    var options = {
-        method: 'POST',
-        url: service_url ,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    function cb(error, response, body) {
-        if (!error) {
-            var info = JSON.parse(JSON.stringify(body));
-            callback(info);
-        }
-        else {
-            console.log('Error happened: '+ error);
-        }
-    }
-    request(options, cb);
-};
 
 
 var post_list = function(input, callback) {
@@ -197,11 +77,6 @@ var get_hot_list = function(num, callback) {
 
 
 module.exports = {
-    get_publish_list: get_publish_list,
-    get_unpublish_list: get_unpublish_list,
-    get_post_view_num: get_post_view_num,
-    post_public:post_public,
-    post_unpublic: post_unpublic,
     post_list: post_list,
     get_list: get_list,
     get_recent_list: get_recent_list,
